ddf3a20e53dcfc52a00ebed60ae4521bea159eb7
Luis Martínez <contacto@luismc.es>
2019-10-27 17:40:01 +0100

    vers.2.1.1

--- src/scripts/009localStorage.js
+++
@@ -1,9 +0,0 @@
-function saveLocalS(key,valor) {
-    return localStorage.setItem(key, valor);
-}
-function getLocalS(key) {
-    return localStorage.getItem(key);
-}
-function delLocalS(key) {
-    return localStorage.removeItem(key);
-}
